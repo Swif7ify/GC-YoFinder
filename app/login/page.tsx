5ad2abd0f3f7daf9346372f5462cd17a547982f8
Earl Ordovez <earl.coding@gmail.com>
2025-08-18 15:15:09 +0800

    added input validation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,9 +4,66 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEyeSlash, faEye, faEnvelope, faLock, faShield, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { toastError, toastSuccess } from "@/utils/toast";
 
 export default function LoginPage() {
+	const validateEmail = () => {
+		const email = form.email.trim();
+		if (!email) {
+			toastError("Email is required.", "Please enter your email.");
+			return false;
+		}
+		if (!/\S+@\S+\.\S+/.test(email)) {
+			toastError("Email is invalid.", "Please enter a valid email address.");
+			return false;
+		}
+		return true;
+	};
+
+	const validatePassword = () => {
+		const password = form.password.trim();
+		if (!password) {
+			toastError("Password is required.", "Please enter your password.");
+			return false;
+		}
+		if (!/(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/.test(password)) {
+			toastError(
+				"Invalid Password",
+				"Password must be at least 8 characters long and include both letters and numbers."
+			);
+			return false;
+		}
+		return true;
+	};
+
 	const [showPassword, setShowPassword] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [form, setform] = useState({
+		email: "",
+		password: "",
+	});
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (isSubmitting) return;
+		setIsSubmitting(true);
+
+		if (!validateEmail()) {
+			return;
+		}
+		if (!validatePassword()) {
+			return;
+		}
+		try {
+			toastSuccess("Successfully signed in!", "Welcome back.");
+			// do something
+		} catch (error) {
+			console.log(error);
+			toastError("Please try again.", "An error occurred while signing in.");
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
 
 	return (
 		<div className="relative">
@@ -23,6 +80,7 @@ export default function LoginPage() {
 
 			{/* form */}
 			<form
+				onSubmit={handleSubmit}
 				className="w-full max-w-md mx-auto mt-12 bg-white/95 shadow-lg rounded-xl p-8"
 				aria-label="Gordon College sign in"
 			>
@@ -43,6 +101,7 @@ export default function LoginPage() {
 								id="email"
 								name="email"
 								type="email"
+								onChange={(e) => setform({ ...form, email: e.target.value })}
 								required
 								autoComplete="username"
 								placeholder="you@student.gordon.edu"
@@ -58,18 +117,19 @@ export default function LoginPage() {
 							<span className="absolute inset-y-0 left-0 pl-3 flex items-center text-gray-400">
 								<FontAwesomeIcon icon={faLock} />
 							</span>
-
 							<input
 								id="password"
 								name="password"
 								type={`${showPassword ? "text" : "password"}`}
+								pattern="(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}"
+								title="At least 8 characters, including letters and numbers"
+								onChange={(e) => setform({ ...form, password: e.target.value })}
 								required
 								autoComplete="current-password"
 								placeholder="Enter your password"
 								className="w-full pl-10 pr-12 py-2 border rounded-md border-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-600 transition"
 								aria-label="Password"
 							/>
-
 							<button
 								type="button"
 								className="absolute inset-y-0 right-0 pr-4 flex items-center text-gray-500"
@@ -104,11 +164,11 @@ export default function LoginPage() {
 
 			<div className="flex flex-row max-w-md justify-center items-center gap-4 mt-6 mx-auto">
 				<div className="w-full bg-white/95 shadow-lg rounded-xl p-4 flex flex-col items-center">
-					<FontAwesomeIcon icon={faMagnifyingGlass} className="text-blue-600 text-2xl mb-2" />
+					<FontAwesomeIcon icon={faMagnifyingGlass} className="text-blue-600 text-2xl h-6 w-6 mb-2" />
 					<h1 className="text-gray-900">Find Items</h1>
 				</div>
 				<div className="w-full bg-white/95 shadow-lg rounded-xl p-4 flex flex-col items-center">
-					<FontAwesomeIcon icon={faShield} className="text-green-600 text-2xl mb-2" />
+					<FontAwesomeIcon icon={faShield} className="text-green-600 text-2xl h-6 w-6 mb-2" />
 					<h1 className="text-gray-900">Secure Claims</h1>
 				</div>
 			</div>
